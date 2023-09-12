b233dd7f9e8994a5fd311893752c240afe874056
JayaMadhuriKaki <jaya.madhuri@nucleusteq.com>
2023-09-12 22:15:58 +0530

    cleared checkstyle errors

--- Capstone/BackEnd/assessmentPortal/src/main/java/com/capstone/assessmentPortal/customAnnotations/AdultAge.java
+++ Capstone/BackEnd/assessmentPortal/src/main/java/com/capstone/assessmentPortal/customAnnotations/AdultAge.java
@@ -7,12 +7,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation interface to check dateofbirth is 18years above or not.
+*/
+
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = AgeValidator.class)
 public @interface AdultAge {
+    /**
+     * default message.
+     * @return message
+    */
     String message() default "Age must be at least 18 years";
+    /**
+     * default groups.
+     * @return groups
+    */
     Class<?>[] groups() default {};
+    /**
+     * default payload.
+     * @return payload
+    */
     Class<? extends Payload>[] payload() default {};
 }
 
--- Capstone/BackEnd/assessmentPortal/src/main/java/com/capstone/assessmentPortal/customAnnotations/AgeValidator.java
+++ Capstone/BackEnd/assessmentPortal/src/main/java/com/capstone/assessmentPortal/customAnnotations/AgeValidator.java
@@ -5,22 +5,27 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 import java.time.Period;
 
+/**
+ * age validatior class.
+*/
+
 public class AgeValidator implements ConstraintValidator<AdultAge, String> {
     @Override
-    public void initialize(AdultAge adultAge) {
+    public void initialize(final AdultAge adultAge) {
     }
 
     @Override
-    public boolean isValid(String dateOfBirth, ConstraintValidatorContext context) {
+    public final boolean isValid(final String dateOfBirth,
+            final ConstraintValidatorContext context) {
         if (dateOfBirth == null) {
             return false; // Let @NotBlank handle this case
         }
-
+        final float minAge = 18;
         try {
             LocalDate birthDate = LocalDate.parse(dateOfBirth);
             LocalDate currentDate = LocalDate.now();
             Period age = Period.between(birthDate, currentDate);
-            return age.getYears() >= 18;
+            return age.getYears() >= minAge;
         } catch (Exception e) {
             return false; // Date parsing error, let @Pattern handle this
         }
